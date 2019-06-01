09c1d82ff6f3b1f693277478efdb9dee04851f44
sebastian <sebastian.ferngren@gmail.com>
2019-06-01 11:55:46 +0200

    Refactor, moves class Stem out of ClefViewModel

--- SheetLearner/Music/ViewModels/ClefViewModel.cs
+++ SheetLearner/Music/ViewModels/ClefViewModel.cs
@@ -75,6 +75,44 @@ namespace SheetLearner.Music.ViewModels
 		}
 	}
 
+	public class Stem
+	{
+		public enum Horizontal
+		{
+			Left,
+			Right,
+			Mid
+		}
+
+		public enum Direction
+		{
+			Up,
+			Down
+		}
+
+		public int PosX()
+		{
+			if (HorizontalOrientaion == Horizontal.Mid)
+				return 14;
+			else if (HorizontalOrientaion == Horizontal.Left)
+				return 3;
+
+			return 14;
+		}
+
+		public int Start()
+		{
+			var offsets = StemDirection == Direction.Up ?
+				new { x = 3, y = 3, noteIndexCorrection = 0 } :
+				new { x = 14, y = ClefViewModel.NoteHeight, noteIndexCorrection = -2 };
+
+			return offsets.y;
+		}
+
+		public Horizontal HorizontalOrientaion { get; set; }
+		public Direction StemDirection { get; set; }
+	}
+
 	public class ClefViewModel : ViewModelBase
 	{
 		public List<NoteSection> Sections;
@@ -163,48 +201,6 @@ namespace SheetLearner.Music.ViewModels
 				note.StemY = note.Y + offsets.y;
 				note.StemX = note.X + offsets.x;
 			}
-		}
-
-		internal class Stem
-		{
-			public enum Horizontal
-			{
-				Left,
-				Right,
-				Mid
-			}
-
-			public enum Direction
-			{
-				Up,
-				Down
-			}
-
-			public int PosX()
-			{
-				if (HorizontalOrientaion == Horizontal.Mid)
-					return 14;
-				else if (HorizontalOrientaion == Horizontal.Left)
-					return 3;
-
-				return 14;
-			}
-
-			public int Start()
-			{
-				var offsets = StemDirection == Direction.Up ?
-					new { x = 3, y = 3, noteIndexCorrection = 0 } :
-					new { x = 14, y = NoteHeight, noteIndexCorrection = -2 };
-
-				return offsets.y;
-			}
-
-			public Horizontal HorizontalOrientaion { get; set; }
-			public Direction StemDirection { get; set; }
-
-			public int Top { get; set; }
-			public int Bottom { get; set; }
-			public int Left { get; set; }
 		}
 
 		private Stem CreateStemForChord(ChordSection chord)
